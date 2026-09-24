7dba38e95463ebf8d6b27273d25babdf4b297468
Flavavisio <flavio.a.rosa87@gmail.com>
2026-09-24 19:54:06 +0100

    Refresh PWA cache after team module fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'condominio-facil-v4';
+const CACHE = 'condominio-facil-v5';
 const CORE = ['./','./index.html','./manifest.webmanifest','./assets/icons/cf-icon.svg'];
 
 self.addEventListener('install', event => {

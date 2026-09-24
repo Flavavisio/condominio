2f03c17671d0bcbd2a7c366c1853a4843323967f
Flavavisio <flavio.a.rosa87@gmail.com>
2026-09-24 17:08:55 +0100

    Bust service worker cache after team crash fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'condominio-facil-v2';
+const CACHE = 'condominio-facil-v3';
 const CORE = ['./','./index.html','./manifest.webmanifest','./assets/icons/cf-icon.svg'];
 
 self.addEventListener('install', event => {

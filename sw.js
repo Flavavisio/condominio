2dc171586195dbdd5b3fa2624adc223a330a9bdd
Flavavisio <flavio.a.rosa87@gmail.com>
2026-09-24 20:32:36 +0100

    Refresh PWA cache for team account creation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'condominio-facil-v5';
+const CACHE = 'condominio-facil-v6';
 const CORE = ['./','./index.html','./manifest.webmanifest','./assets/icons/cf-icon.svg'];
 
 self.addEventListener('install', event => {
